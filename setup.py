b86bd5e387d5918616d43be8f46aa5536ed30159
Austin Bingham <austin@sixty-north.com>
2016-07-27 21:58:05 +0200

    Added some documentation.

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from Cython.Distutils import build_ext
 
 ext_modules = [Extension("hello", ["hello.pyx"])]
 
+# This is the important part. By setting this compiler directive, cython will
+# embed signature information in docstrings. Sphinx then knows how to extract
+# and use those signatures.
 for e in ext_modules:
     e.cython_directives = {"embedsignature": True}
 
